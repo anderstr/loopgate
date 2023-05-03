73c26e5fcfe5c2375344667ad251285b741114ab
SexyEarl <taa435@gmail.com>
2023-05-03 10:26:16 -0700

    Updated config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -24,20 +24,7 @@ const unlockables: Unlockable[] = [
     cid: "bafybeihzee6e6xfncnbrpikzfd32kcqoowj3m7tdx742upgw75avdugmta", // HTML blog example
     nftId: [
       "0xf6491a430f212762f6612f94fcf1f2d9aadabe1fc3bb4b42a8aaa2998adce25a",
-    ], // You need to own 'GM #1' to unlock this.
-  },
-  {
-    cid: "bafybeihx5eacyxeydcpvudwxa242rnjhn67femy46gzas5d2djb24ti5mi", // MP4 video example
-    nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-    ], // You need to own 'GM #2' to unlock this.
-  },
-  {
-    cid: "bafybeihhx5v3saq3b7n55ub5q3atuw2udbqc5ictkv2ih7vd3hxptu22nu", // Web game example
-    nftId: [
-      "0x8aa9d39f44b4b8488d0bbf04ea12bec99ddbe676a1b9a38d853701327437e78c",
-      "0x3d483f631a391a3706446613929d253cfddcb47900a07593c5004c5e3827d9ee",
-    ], // You need to own 'GM #1' & 'GM #2' to unlock this.
+    ], // You need to own 'Salvations Edge' to unlock this.
   },
 ];
 
